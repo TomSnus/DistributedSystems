8088a6a47b495841488b59f4392ed5a6728e3223
TomSnus <senteswins@gmail.com>
2017-05-29 14:00:36 +0100

    Marshalling

--- 7. REST/src/main/java/de/oth/vs/server/MarshallExample.java
+++ 7. REST/src/main/java/de/oth/vs/server/MarshallExample.java
@@ -2,12 +2,7 @@ package de.oth.vs.server;
 
 import de.oth.vs.entity.Address;
 import de.oth.vs.entity.Student;
-import org.eclipse.persistence.jaxb.JAXBMarshaller;
-
-import javax.xml.bind.JAXB;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
+import javax.xml.bind.*;
 import java.io.File;
 
 /**
@@ -15,12 +10,27 @@ import java.io.File;
  */
 public class MarshallExample {
 
-    public static void main(String[] args) throws JAXBException {
+    public static void main(String[] args)  {
+    
+        try {
+            System.setProperty("javax.xml.bind.context.factory","org.eclipse.persistence.jaxb.JAXBContextFactory");
+            JAXBContext context = JAXBContext.newInstance(Student.class);
+            JAXB.marshal(new Student(666, "Max", 250, new Address("Test", "Regensburg")), new File("student.xml"));
+            Student example = JAXB.unmarshal("D:/Drive/6. Semester/Allgemeine Informatik_6.Semester/VS/DistributedSystems/student.xml", Student.class);
+            System.out.println(example);
 
-        JAXBContext context = JAXBContext.newInstance(Student.class);
-        JAXB.marshal(new Student(666, "Max", 250, new Address("Test", "Regensburg")), new File("student.xml"));
-        Student example = JAXB.unmarshal("D:/Drive/6. Semester/Allgemeine Informatik_6.Semester/VS/DistributedSystems/student.xml", Student.class);
-        System.out.println(example);
+            //JSON
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshaller.setProperty("eclipselink.media-type", "application/json");
+            //  marshaller.setProperty(MarshallerProperties.JSON_INCLUDE_ROOT, false);
+            marshaller.marshal(example, System.out);
+        } catch (PropertyException e) {
+            e.printStackTrace();
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
